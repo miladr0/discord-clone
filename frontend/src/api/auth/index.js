8d931cf6356da29b8f4c98c4f8baa51e7cd6b269
miladr0 <miladr0r@gmail.com>
2022-06-10 01:39:01 +0430

    refresh token
    redirect to login if refresh token expierd

--- frontend/src/api/auth/index.js
+++ frontend/src/api/auth/index.js
@@ -1,10 +1,11 @@
+import { LOGIN_URL } from '../../constants/history.constants'
 import client from '../client'
 
 export const register = (data) => {
   return client.post('/api/v1/auth/register', data)
 }
 export const login = (data) => {
-  return client.post('/api/v1/auth/login', data)
+  return client.post(LOGIN_URL, data)
 }
 
 export const logout = (refreshToken) => {
--- frontend/src/constants/history.constants.js
+++ frontend/src/constants/history.constants.js
@@ -2,3 +2,5 @@ export const HOME_PAGE = '/channels'
 export const ME_PAGE = '/channels/me'
 export const LOGIN_PAGE = '/login'
 export const DM_URL = (dmId = ':dmId') => `/channels/me/${dmId}`
+export const LOGIN_URL = '/api/v1/auth/login'
+export const REFRESH_TOKEN_URL = '/api/v1/auth/refresh-tokens'
