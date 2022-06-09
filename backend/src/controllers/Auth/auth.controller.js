fb519490776d02d2c11aa380170365e99a16430a
miladr0 <miladr0r@gmail.com>
2022-06-10 01:38:12 +0430

    pass user object when refresh token

--- backend/src/controllers/Auth/auth.controller.js
+++ backend/src/controllers/Auth/auth.controller.js
@@ -21,8 +21,8 @@ const logout = catchAsync(async (req, res) => {
 });
 
 const refreshTokens = catchAsync(async (req, res) => {
-  const tokens = await authService.refreshAuth(req.body.refreshToken);
-  res.send({ ...tokens });
+  const result = await authService.refreshAuth(req.body.refreshToken);
+  res.send({ ...result });
 });
 
 const forgotPassword = catchAsync(async (req, res) => {
